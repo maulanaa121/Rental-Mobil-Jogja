830b769f38758d2332b2d6e1de7310c2aaff1b32
maulanaa121 <fm886211@gmail.com>
2026-09-17 13:19:46 +0700

    fix: add component about

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../Navbar/Navbar";
 import Hero from "../Hero/Hero";
+import About from "../About/About";
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
+            <About/>
         </>
     );
 };
