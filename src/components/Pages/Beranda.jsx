06d2098ad97ec9baf9b2defa7852e4c50670b296
maulanaa121 <fm886211@gmail.com>
2026-09-18 14:17:49 +0700

    fix: move MobilUnggulanPage n replace to berandaPage

--- src/components/Pages/Beranda.jsx
+++ src/components/Pages/Beranda.jsx
@@ -5,6 +5,7 @@ import VisiMisi from "../VisiMisi/VisiMisi";
 import Footer from "../Footer/Footer";
 import HeroBeranda from "../Beranda/HeroBeranda";
 import Keunggulan from "../Beranda/Keunggulan";
+import MobilUnggulan from "../Beranda/MobilUnggulan";
 
 const Beranda = () => {
     return (
@@ -12,6 +13,7 @@ const Beranda = () => {
             <Navbar />
             <HeroBeranda/>
             <Keunggulan/>
+            <MobilUnggulan/>
             <Footer/>
         </>
     );
