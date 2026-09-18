8857fee02362f2cdffac534139195e5c33aee46d
maulanaa121 <fm886211@gmail.com>
2026-09-18 14:14:27 +0700

    fix: move PagesUnggulan n replace to BerandaPages

--- src/components/Pages/Beranda.jsx
+++ src/components/Pages/Beranda.jsx
@@ -4,12 +4,14 @@ import About from "../About/About";
 import VisiMisi from "../VisiMisi/VisiMisi";
 import Footer from "../Footer/Footer";
 import HeroBeranda from "../Beranda/HeroBeranda";
+import Keunggulan from "../Beranda/Keunggulan";
 
 const Beranda = () => {
     return (
         <>
             <Navbar />
             <HeroBeranda/>
+            <Keunggulan/>
             <Footer/>
         </>
     );
